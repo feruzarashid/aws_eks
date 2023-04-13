2e00eca63bace8a0577dffd87f6017f6ddc4f439
feruzarashid <bakhferuza@@yahoo.com>
2023-04-13 00:24:43 +0000

    kubectl installation

---
+++ kubectl.sh
@@ -0,0 +1,4 @@
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.2/2023-03-17/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bash_profile
